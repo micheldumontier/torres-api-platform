9d58a3978bc51b3e3f1e2ada387f2bbc1bc47f87
Michel Dumontier <michel.dumontier@gmail.com>
2020-11-13 13:32:11 +0100

    added windows version of graphql query test

--- api/data/curl.bat
+++ api/data/curl.bat
@@ -1,2 +1,3 @@
 curl -X POST "https://localhost:8443/datasets" -H "accept: application/ld+json" -H "content-type: application/ld+json" -k -T "dataset.jsonld"
-curl.exe -X POST "https://localhost:8443/data_services" -H "accept: application/ld+json" -H "content-type: application/ld+json" -k -T "dataservice.jsonld"
+curl -X POST "https://localhost:8443/data_services" -H "accept: application/ld+json" -H "content-type: application/ld+json" -k -T "dataservice.jsonld"
+curl.exe -X POST "https://localhost:8443/graphql" -k -H "Content-Type: application/json" -d "{\"query\": \"{ datasets { edges { node { title } } } } \"}"
